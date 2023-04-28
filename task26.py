4676152eb0c08abdf6763c9d180b10f436bf8ca2
Nikolai <agro034@mail.ru>
2023-04-29 00:56:01 +0300

    Работает нормально

---
+++ task26.py
@@ -0,0 +1,12 @@
+x = int(input('Введите число: '))
+y = int(input('Введите показатель степени: '))
+
+def nums_degree(a, n):
+    
+    if n == 0:
+        return 1
+    # elif n == 1:
+    #     return a
+      
+    return a* nums_degree(a,n-1) 
+print(nums_degree(x,y))
